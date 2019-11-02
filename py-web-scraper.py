1fd6def372f819377f55040bb1c455db4a953b1b
jakoobishak <jakoob_ishak@hotmail.com>
2019-11-02 09:05:15 +0100

    write to csv

--- py-web-scraper.py
+++ py-web-scraper.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 
@@ -8,27 +9,31 @@ soup = BeautifulSoup(source, "lxml")
 
 tbody = soup.find("tbody", attrs={"class": "searchable"})
 
-try:
-    for table in tbody.find_all("tr", attrs={"class": "table-primary"}):
+with open("stream-links.csv", "w") as f:
+    f = csv.writer(f, delimiter=",")
+    f.writerow(["title", "game", "day", "date", "link"])
 
-        cols = table.find_all("td")
-        cols = [x.text.strip() for x in cols]
-        video = table.find("a")["href"]
+    try:
+        for table in tbody.find_all("tr", attrs={"class": "table-primary"}):
 
-        print(cols[0] + " - " + cols[1] + " - " + cols[4] + " - " + video)
-        match_day = cols[0]
+            cols = table.find_all("td")
+            cols = [x.text.strip() for x in cols]
+            video = table.find("a")["href"]
 
-        for sec_table in tbody.find_all("tr", attrs={"class": "table-secondary"}):
-            sec = sec_table.find_all("td")
-            sec = [y.text.strip() for y in sec]
-            if sec[0] == match_day:
-                print(sec[3])
+            f.writerow([cols[4], "games", cols[1], cols[0], video])
+            print(cols[4] + " - " + cols[1] + " - " + cols[0] + " - " + video)
+            match_day = cols[0]
 
-        print()
+            for sec_table in tbody.find_all("tr", attrs={"class": "table-secondary"}):
+                sec = sec_table.find_all("td")
+                sec = [y.text.strip() for y in sec]
+                if sec[0] == match_day:
+                    print(sec[3])
+                    f.writerow(["", sec[3]])
+            print()
 
-
-except Exception as e:
-    cols = None
-    video = None
-    sec = None
+    except Exception as e:
+        cols = None
+        video = None
+        sec = None
 
